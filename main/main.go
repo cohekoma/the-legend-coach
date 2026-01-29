06fadd93ce2a89776a27ebd1d28d98f424026dd9
cohekoma <dinhhoangbaochan@gmail.com>
2026-01-29 22:01:20 +0700

    add sample for team and player

--- main/main.go
+++ main/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Player struct {
 	Name     string
 	Position string
@@ -12,5 +14,17 @@ type Team struct {
 }
 
 func main() {
+	p1 := Player{
+		Name:     "Bruno Fernandes",
+		Position: "CAM",
+		Overall:  88,
+	}
 
+	t1 := Team{
+		Name:    "Manchester United",
+		Players: []Player{p1},
+	}
+
+	fmt.Printf("This is %s\n", t1.Name)
+	fmt.Printf("The captain of the team is %s, he's playing at the role %s and he's rated with %d in overall!\n", p1.Name, p1.Position, p1.Overall)
 }
